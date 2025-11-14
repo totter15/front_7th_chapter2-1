2696d0ce04027251dfde3da92860c553d8dc2127
천진아 <jncheon@carmerce.co.kr>
2025-11-15 00:12:40 +0900

    chore: test

--- src/component/CartModal.js
+++ src/component/CartModal.js
@@ -41,6 +41,7 @@ const CartModal = ({ checkedCartItems }) => {
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                       d="M3 3h2l.4 2M7 13h10l4-8H5.4m2.6 8L6 2H3m4 11v6a1 1 0 001 1h1a1 1 0 001-1v-6M13 13v6a1 1 0 001 1h1a1 1 0 001-1v-6"></path>
               </svg>
+              
               장바구니
             </h2>
             <button id="cart-modal-close-btn" class="text-gray-400 hover:text-gray-600 p-1">
